ed61538541b0cdb77338fadf30ad8e939a07e5e7
Dingensen <53115461+Dingensen@users.noreply.github.com>
2019-08-20 15:32:16 +0200

    make rough foundation for client side
    make rough foundation for client side, including client.js, client.html, and client-tests

---
+++ Client/client.js
@@ -0,0 +1,13 @@
+// jshint esversion: 6
+// jshint browser: true
+// jshint node: true
+"use strict";
+
+Vue.config.devtools = true
+
+var timeApp = new Vue({
+  el: '#App',
+  data: {
+    message: "Hello world!"
+  }
+});
